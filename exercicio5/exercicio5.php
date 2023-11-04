cdbae5c1dd1d6b112122b1b2f0d67eae7b07036c
Leonardo Bueno Martins <leo01012005@gmail.com>
2023-11-03 22:27:44 -0300

    exercicio5

---
+++ exercicio5/exercicio5.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $Gremio = 4;
+    $Inter = 2;
+    if ($Gremio < $Inter) {
+        echo"O Inter ganho com $Inter gols";
+    } else {
+        echo "O Gremio ganho com $Gremio gols";
+    }
+    ?>
+</body>
+</html>
